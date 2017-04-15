5426ca23d2bb35ec1ced6bc1f38882206ba82d77
Martin Gaitan <mgaitan@onapsis.com>
2017-04-15 20:01:40 -0300

    static root

--- metrodeclaracion/settings.py
+++ metrodeclaracion/settings.py
@@ -124,5 +124,5 @@ LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'public', 'static')
 STATIC_URL = '/static/'
