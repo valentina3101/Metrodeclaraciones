43826dabbb0094d197ee430141f336d259a803b2
Martin Gaitan <mgaitan@onapsis.com>
2017-04-16 14:19:05 -0300

    debug

--- metrodeclaracion/settings.py
+++ metrodeclaracion/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%^!$2+zax+pl+!k&v_v3ki26q)emd!v%=%t*dwo3=-+xi*ie1_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
